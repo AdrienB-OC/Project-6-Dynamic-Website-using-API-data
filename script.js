db722493f63900da445d67087fffd294cf9f828d
Adrien Burgun <adrienburgun@hotmail.fr>
2021-12-17 01:49:49 +0900

    Ajout fenêtres modales

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function fetchData() {
-	fetch("http://localhost:8000/api/v1/titles/?genre=action&sort_by=-imdb_score")
-	.then(response =>{
+function fetchData(cat_name) {
+	fetch("http://localhost:8000/api/v1/titles/?genre=" + cat_name + "&sort_by=-imdb_score")
+	.then(response => {
 		if (!response.ok) {
 			throw Error('Erreur');
 		}
@@ -29,9 +29,28 @@ function fetchData() {
 		})
 		.then(data => {
 			console.log(data);
-			return `<p> Title : ${data.title}</p>`;
+			return `<button class="modal-btn"><img data-target="${cat_name}${i}" data-toggle="action-modal" src="${data.image_url}"></button>
+
+			        <div id="${cat_name}${i}" class="modal-bg">
+			            <div class="modal">
+			                <div class="movie">
+			                    <img src="${data.image_url}">
+			                    <p><span class="cat-name">Title :</span> ${data.title}</p>
+			                    <p><span class="cat-name">Genre :</span> ${data.genres}</p>
+			                    <p><span class="cat-name">Country :</span> ${data.countries}</p>
+			                    <p><span class="cat-name">Duration :</span> ${data.duration}</p>
+			                    <p><span class="cat-name">Score :</span> ${data.imdb_score}</p>
+			                    <p><span class="cat-name">Rated :</span> ${data.rated}</p>
+			                    <p><span class="cat-name">Actors :</span> ${data.actors}</p>
+			                    <p><span class="cat-name">Grossing (USD):</span> ${data.worldwide_gross_income}</p>
+			                    <p><span class="cat-name">Description :</span> ${data.long_description}</p>
+			                    <span data-dismiss="action-modal" class="modal-close">X</span>
+			                </div>
+			          </div>
+			        </div>`;
 	}).then(html => {
 		console.log(html);
+		document.querySelector("#app").insertAdjacentHTML("beforeend", html);
 	})
 	}})
 	.catch(error => {
@@ -39,8 +58,49 @@ function fetchData() {
 	});
 };
 
-fetchData();
+fetchData('action');
 
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'action-modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            var modal = document.getElementById(m_ID)
+            modal.classList.add('bg-active');
+            e.preventDefault();
+        }
+    }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'action-modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal-bg bg-active"]');
+        modal.classList.remove('bg-active');
+        e.preventDefault();
+    }
+}, false);
+
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'animation-modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            var modal = document.getElementById(m_ID)
+            modal.classList.add('bg-active');
+            e.preventDefault();
+        }
+    }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'animation-modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal-bg bg-active"]');
+        modal.classList.remove('bg-active');
+        e.preventDefault();
+    }
+}, false);
 
 
 /*function fetchData() {
